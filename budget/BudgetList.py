3abaa141ca6c340685c8f38099274d6ad9728754
almost2blank <44064277+almost2blank@users.noreply.github.com>
2021-12-01 20:17:38 -0500

    0

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -20,7 +20,7 @@ class BudgetList():
 
     def __iter__(self):
         iter(self.expenses)
-        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.expenses)
         return self
 
 def main():
