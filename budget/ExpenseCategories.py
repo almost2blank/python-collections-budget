4eaa1675f6e3bd317a9599b2e9b8cebd8804a473
almost2blank <44064277+almost2blank@users.noreply.github.com>
2021-12-02 15:42:38 -0500

    issubset

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -8,10 +8,13 @@ def main():
     divided_set_comp = expenses.categorize_set_comprehension()
 
     if not divided_set_comp == divided_for_loop:
-        print('Sets or NOT equal by == test')
+        print("Sets are NOT equal by == test")
 
     for a,b in zip(divided_for_loop, divided_set_comp):
-        pass
+        if not a.issubset(b):
+            print("Sets are NOT equal by subset test")
+        if not b.issubset(a):
+            print("Sets are NOT equal by subset test")
 
 
 if __name__ == "__main__":
